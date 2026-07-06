4fbdd47197edc15a369eae4232df9c320928b655
Rudra Gupta <rudragupta200408@gmail.com>
2026-07-06 17:34:48 +0530

    Updated submissionService Class

--- client/src/services/submissionService.js
+++ client/src/services/submissionService.js
@@ -1,11 +1,11 @@
 import runnerApi from "./runnerApi";
 
 export async function runSubmission(payload) {
-  const { data } = await runnerApi.post("/submission/run", payload);
+  const { data } = await runnerApi.post("/run", payload);
   return data;
 }
 
 export async function submitSubmission(payload) {
-  const { data } = await runnerApi.post("/submission/submit", payload);
+  const { data } = await runnerApi.post("/submit", payload);
   return data;
 }
